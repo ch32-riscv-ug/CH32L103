8c99ea6896ed70ab4ce1a2de229fb71608068faa
root <akiraneko@gmail.com>
2024-05-15 11:25:14 +0900

    update

--- update.sh
+++ update.sh
@@ -23,7 +23,7 @@ cd ..
 # https://www.wch.cn/downloads/CH32L103EVT_ZIP.html
 wget --continue https://www.wch.cn/downloads/file/455.html -O CH32L103EVT.ZIP
 rm -rfv EVT
-unzip -O utf-8 *.ZIP
+unzip -O GB2312 *.ZIP
 
 git add . --all
 git commit -m "update"
